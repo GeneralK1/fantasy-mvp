7f0736029cf1e906900bcad8f4f34d42f17ffea5
GeneralK1 <georg.dmtr@yandex.ru>
2026-08-08 14:37:48 +0300

    Update server.js

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use((req, res, next) => {
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/auth', authRoutes);
+
 app.use(cors({
   origin: process.env.NODE_ENV === 'production' 
     ? 'https://master-maze-app.online' 
